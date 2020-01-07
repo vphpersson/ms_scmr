65b768820aea30cc6d5a14a40bb5391e0effe939
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-01-07 10:50:07 +0100

    Added RCreateService code.

---
+++ ms_scmr/operations/r_create_service_w/__init__.py
@@ -0,0 +1,204 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from abc import ABC
+from typing import Final, Type, Optional, Tuple
+from struct import pack as struct_pack, unpack as struct_unpack
+
+from rpc.connection import Connection as RPCConnection
+from rpc.ndr import Pointer, UnidimensionalConformantArray, NullPointer, ConformantVaryingString
+from rpc.utils.client_protocol_message import ClientProtocolRequestBase, ClientProtocolResponseBase, obtain_response
+from rpc.utils.ndr import pad as ndr_pad, calculate_pad_length
+
+from ms_scmr.operations.r_create_service_w.exceptions import RCreateServiceWError, RCreateServiceWReturnCode
+from ms_scmr.operations import Operation
+from ms_scmr.structures.service_type import ServiceType
+from ms_scmr.structures.error_control import ErrorControl
+from ms_scmr.structures.start_type import StartType
+from ms_scmr.structures.service_access import ServiceAccessFlagMask
+
+
+class RCreateServiceWRequestBase(ClientProtocolRequestBase, ABC):
+    OPERATION: Final[Operation] = Operation.R_CREATE_SERVICE_W
+
+
+class RCreateServiceWResponseBase(ClientProtocolResponseBase, ABC):
+    ERROR_CLASS: Final[Type[RCreateServiceWError]] = RCreateServiceWError
+
+
+@dataclass
+class RCreateServiceWRequest(RCreateServiceWRequestBase):
+    scm_handle: bytes
+    service_name: str
+    display_name: str
+    desired_access: ServiceAccessFlagMask
+    service_type: ServiceType
+    start_type: StartType
+    error_control: ErrorControl
+    binary_path_name: str
+    load_order_group: Optional[str] = None
+    tag_id: int = 0
+    dependencies: Tuple[str, ...] = ()
+    # TODO: Where are these stored?
+    service_start_name: Optional[str] = None
+    password: Optional[str] = None
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> RCreateServiceWRequest:
+
+        attribute_name_class_pair = (
+            ('service_name', ConformantVaryingString),
+            ('display_name', ConformantVaryingString),
+            ('desired_access', ServiceAccessFlagMask.from_mask),
+            ('service_type', ServiceType),
+            ('start_type', StartType),
+            ('error_control', ErrorControl),
+            ('binary_path_name', ConformantVaryingString),
+            ('load_order_group', ConformantVaryingString),
+            ('tag_id', None),
+            ('dependencies', UnidimensionalConformantArray),
+            ('_depen_size', None),
+            ('service_start_name', ConformantVaryingString),
+            ('password', UnidimensionalConformantArray),
+            ('_pw_size', None)
+        )
+        kwargs = {}
+        offset = 20
+        for attribute_name, attribute_class in attribute_name_class_pair:
+            if attribute_name in {'_depen_size', '_pw_size'}:
+                offset += 4
+                continue
+            if attribute_name in {'service_name', 'binary_path_name'}:
+                ndr_string = attribute_class.from_bytes(data=data[offset:])
+                kwargs[attribute_name] = ndr_string.representation
+                offset += calculate_pad_length(length_unpadded=len(ndr_string))
+            elif attribute_name in {'service_type', 'start_type', 'error_control', 'desired_access'}:
+                kwargs[attribute_name] = attribute_class(struct_unpack('<I', data[offset:offset+4])[0])
+                offset += 4
+            elif attribute_name == 'tag_id':
+                kwargs[attribute_name] = struct_unpack('<I', data[offset:offset+4])[0]
+                offset += 4
+            else:
+                attribute_pointer = Pointer.from_bytes(data=data[offset:])
+                attribute_ndr = (
+                    attribute_class.from_bytes(data=attribute_pointer.representation)
+                    if not isinstance(attribute_pointer, NullPointer) else None
+                )
+                attribute_len = len(attribute_ndr) if attribute_ndr is not None else 0
+
+                if attribute_name == 'dependencies' and attribute_ndr is not None:
+                    kwargs[attribute_name] = tuple(
+                        b''.join(attribute_ndr.representation).decode(encoding='utf-16-le').split('\x00')
+                    ) if attribute_ndr else tuple()
+                elif attribute_name == 'password' and attribute_ndr is not None:
+                    kwargs[attribute_name] = b''.join(attribute_ndr.representation).decode(encoding='utf-16-le').rstrip(
+                        '\x00')
+                else:
+                    kwargs[attribute_name] = attribute_ndr.representation if attribute_ndr is not None else None
+
+                offset += calculate_pad_length(length_unpadded=attribute_pointer.structure_size+attribute_len)
+
+        return cls(
+            scm_handle=data[:20],
+            **kwargs
+        )
+
+    def __bytes__(self) -> bytes:
+        dependencies_ndr = Pointer(
+            representation=UnidimensionalConformantArray(
+                representation=tuple(
+                    bytes([byte])
+                    for byte in '\x00'.join(self.dependencies).encode(encoding='utf-16-le')
+                )
+            )
+        ) if len(self.dependencies) != 0 else NullPointer()
+
+        password_ndr = Pointer(
+            representation=UnidimensionalConformantArray(
+                representation=tuple(
+                    bytes([byte])
+                    for byte in (self.password + '\x00').encode(encoding='utf-16-le')
+                )
+            )
+        ) if self.password is not None else NullPointer()
+
+        return b''.join([
+            self.scm_handle,
+            ndr_pad(data=bytes(ConformantVaryingString(representation=self.service_name))),
+            ndr_pad(data=bytes(Pointer(representation=ConformantVaryingString(representation=self.display_name)))),
+            struct_pack('<I', self.desired_access.to_mask()),
+            struct_pack('<I', self.service_type.value),
+            struct_pack('<I', self.start_type.value),
+            struct_pack('<I', self.error_control.value),
+            ndr_pad(data=bytes(ConformantVaryingString(representation=self.binary_path_name))),
+            ndr_pad(
+                data=bytes(
+                    Pointer(representation=ConformantVaryingString(representation=self.load_order_group))
+                    if self.load_order_group is not None else NullPointer()
+                )
+            ),
+            struct_pack('<I', self.tag_id),
+            ndr_pad(data=bytes(dependencies_ndr)),
+            struct_pack(
+                '<I',
+                (0 if isinstance(dependencies_ndr, NullPointer) else len(
+                    dependencies_ndr.representation) - UnidimensionalConformantArray.STRUCTURE_SIZE)
+            ),
+            ndr_pad(
+                data=bytes(
+                    Pointer(representation=ConformantVaryingString(representation=self.service_start_name))
+                    if self.service_start_name is not None else NullPointer()
+                )
+            ),
+            ndr_pad(data=bytes(password_ndr)),
+            struct_pack(
+                '<I',
+                (0 if isinstance(password_ndr, NullPointer) else len(
+                    password_ndr.representation) - UnidimensionalConformantArray.STRUCTURE_SIZE)
+            )
+        ])
+
+
+@dataclass
+class RCreateServiceWResponse(RCreateServiceWResponseBase):
+    tag_id: int
+    service_handle: bytes
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> RCreateServiceWResponse:
+        return cls(
+            tag_id=struct_unpack('<I', data[:4])[0],
+            service_handle=data[4:24],
+            return_code=RCreateServiceWReturnCode(struct_unpack('<I', data[24:28])[0])
+        )
+
+    def __bytes__(self) -> bytes:
+        return b''.join([
+            struct_pack('<I', self.tag_id),
+            self.service_handle,
+            struct_pack('<I', self.return_code.value)
+        ])
+
+
+RCreateServiceWResponseBase.REQUEST_CLASS = RCreateServiceWRequest
+RCreateServiceWRequestBase.RESPONSE_CLASS = RCreateServiceWResponse
+
+
+async def r_create_service_w(
+    rpc_connection: RPCConnection,
+    request: RCreateServiceWRequest,
+    raise_exception: bool = True
+) -> RCreateServiceWResponse:
+    """
+    Perform the RCreateServiceW operation.
+
+    :param rpc_connection:
+    :param request:
+    :param raise_exception:
+    :return:
+    """
+
+    return await obtain_response(rpc_connection=rpc_connection, request=request, raise_exception=raise_exception)
+
+RCreateServiceWRequest.from_bytes(
+    data=bytes.fromhex('00000000a159a88204ddd347869c7d27737eb46805000000000000000500000057004800410054000000aaaa074700000a000000000000000a0000004900530047004f0049004e0047004f004e000000ff010f0010000000020000000000000011000000000000001100000043003a005c0077006800650065006c00650064005c0068006f007200730065000000bfbf00000000000000000000000000000000000000000000000000000000')
+)
---
+++ ms_scmr/operations/r_create_service_w/exceptions.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+from abc import ABC
+from typing import Dict, Type
+from enum import IntEnum
+
+from ms_scmr.base import MSSCMRResponseError
+
+
+class RCreateServiceWReturnCode(IntEnum):
+    ERROR_ACCESS_DENIED = 5
+    ERROR_INVALID_HANDLE = 6
+    ERROR_INVALID_DATA = 13
+    ERROR_INVALID_PARAMETER = 87
+    ERROR_INVALID_NAME = 123
+    ERROR_INVALID_SERVICE_ACCOUNT = 1057
+    ERROR_CIRCULAR_DEPENDENCY = 1059
+    ERROR_SERVICE_MARKED_FOR_DELETE = 1072
+    ERROR_SERVICE_EXISTS = 1073
+    ERROR_DUPLICATE_SERVICE_NAME = 1078
+    ERROR_SHUTDOWN_IN_PROGRESS = 1115
+
+
+class RCreateServiceWError(MSSCMRResponseError, ABC):
+    RETURN_CODE_TO_ERROR_CLASS: Dict[RCreateServiceWReturnCode, Type[RCreateServiceWError]] = {}
+
+
+class AccessDeniedError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_ACCESS_DENIED
+    DESCRIPTION = 'The SC_MANAGER_CREATE_SERVICE access right had not been granted to the caller when the RPC context handle was created.'
+
+
+class InvalidHandleError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_INVALID_HANDLE
+    DESCRIPTION = 'The handle specified is invalid.'
+
+
+class InvalidDataError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_INVALID_DATA
+    DESCRIPTION = 'The data is invalid.'
+
+
+class InvalidParameterError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_INVALID_PARAMETER
+    DESCRIPTION = 'A parameter that was specified is invalid.'
+
+
+class InvalidNameError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_INVALID_NAME
+    DESCRIPTION = 'The specified service name is invalid.'
+
+
+class InvalidServiceAccountError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_INVALID_SERVICE_ACCOUNT
+    DESCRIPTION = 'The user account name specified in the lpServiceStartName parameter does not exist.'
+
+
+class CircularDependencyError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_CIRCULAR_DEPENDENCY
+    DESCRIPTION = 'A circular service dependency was specified.'
+
+
+class ServiceMarkedForDeleteError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_SERVICE_MARKED_FOR_DELETE
+    DESCRIPTION = 'The service record with a specified name already exists and RDeleteService has been called for it.'
+
+
+class ServiceExistsError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_SERVICE_EXISTS
+    DESCRIPTION = 'The service record with the ServiceName matching the specified lpServiceName already exists.'
+
+
+class DuplicateServiceNameError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_DUPLICATE_SERVICE_NAME
+    DESCRIPTION = 'The service record with the same DisplayName or the same ServiceName as the passed in lpDisplayName already exists in the service control manager database.'
+
+
+class ShutdownInProgressError(RCreateServiceWError):
+    RETURN_CODE = RCreateServiceWReturnCode.ERROR_SHUTDOWN_IN_PROGRESS
+    DESCRIPTION = 'The system is shutting down.'
+
+
+RCreateServiceWError.RETURN_CODE_TO_ERROR_CLASS.update({
+    RCreateServiceWReturnCode.ERROR_ACCESS_DENIED: AccessDeniedError,
+    RCreateServiceWReturnCode.ERROR_INVALID_HANDLE: InvalidHandleError,
+    RCreateServiceWReturnCode.ERROR_INVALID_DATA: InvalidDataError,
+    RCreateServiceWReturnCode.ERROR_INVALID_PARAMETER: InvalidParameterError,
+    RCreateServiceWReturnCode.ERROR_INVALID_NAME: InvalidNameError,
+    RCreateServiceWReturnCode.ERROR_INVALID_SERVICE_ACCOUNT: InvalidServiceAccountError,
+    RCreateServiceWReturnCode.ERROR_CIRCULAR_DEPENDENCY: CircularDependencyError,
+    RCreateServiceWReturnCode.ERROR_SERVICE_MARKED_FOR_DELETE: ServiceMarkedForDeleteError,
+    RCreateServiceWReturnCode.ERROR_SERVICE_EXISTS: ServiceExistsError,
+    RCreateServiceWReturnCode.ERROR_DUPLICATE_SERVICE_NAME: DuplicateServiceNameError,
+    RCreateServiceWReturnCode.ERROR_SHUTDOWN_IN_PROGRESS: ShutdownInProgressError
+})
