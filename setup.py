09b440eb974bb5cb0fc5fd75e757e16b833fe24e
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2022-08-04 01:03:55 +0200

    Fix references.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 setup(
     name='ms_scmr',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
-        'rpc @ git+ssh://git@github.com/vphpersson/rpc.git#egg=rpc',
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/ndr.git#egg=ndr'
+        'rpc @ git+https://github.com/vphpersson/rpc.git#egg=rpc',
+        'msdsalgs @ git+https://github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
+        'ndr @ git+https://github.com/vphpersson/ndr.git#egg=ndr'
     ]
 )
